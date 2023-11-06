abbbdbf1bfef2e093d77491c9257b59235efe994
Aftab Usmani <usmani12345@gmail.com>
2023-11-05 23:11:32 -0800

    Added more cpde to Dcache.

--- src/main/scala/memory/dCache.scala
+++ src/main/scala/memory/dCache.scala
@@ -7,20 +7,42 @@ class dCacheCmdBundle extends Bundle {
   val write   = Bool()
 }
 
-class dCache extends Module with RequireAsyncReset {
-  val requestor = IO(Vec(8,Flipped(DecoupledIO(new dCacheCmdBundle))))
+class dCache (val NUM_PORTS: Int = 8) extends Module with RequireAsyncReset {
+  val requestor = IO(Vec(NUM_PORTS,Flipped(DecoupledIO(new dCacheCmdBundle))))
+  val rData = IO(Output(Vec(NUM_PORTS,ValidIO(UInt(32.W)))))
+  val cmdQueuesInstAry = Seq.fill(NUM_PORTS)(Module(new Queue(new dCacheCmdBundle, 4)))
+  val cmdArbiterInst = Module(new RRArbiter(new dCacheCmdBundle,NUM_PORTS))
 
-  val cmdQueuesAry = Seq.fill(8)(Module(new Queue(new dCacheCmdBundle, 4)))
-  requestor.zipWithIndex.map{case(x,y) =>
-    x <> cmdQueuesAry(y).io.enq
+  requestor.zipWithIndex.map { case (x, y) =>
+    x <> cmdQueuesInstAry(y).io.enq
   }
-  
-  cmdQueuesAry(0).io.deq <> cmdOut(0)
-  cmdQueuesAry(1).io := DontCare
-  cmdQueuesAry(2).io := DontCare
-  cmdQueuesAry(3).io := DontCare
-  cmdQueuesAry(4).io := DontCare
-  cmdQueuesAry(5).io := DontCare
-  cmdQueuesAry(6).io := DontCare
-  cmdQueuesAry(7).io := DontCare
+  cmdQueuesInstAry.zipWithIndex.map { case (queue, num) =>
+    cmdArbiterInst.io.in(num) <> queue.io.deq
+  }
+  val arbChosenPipe = RegNext(cmdArbiterInst.io.chosen)
+
+  val mem = SyncReadMem(8192,UInt(32.W))
+  val memDataOut = Wire(UInt(32.W))
+  //Single Port SRAM
+  memDataOut := DontCare
+  when(cmdArbiterInst.io.out.valid) {
+    val rdwrPort = mem(cmdArbiterInst.io.out.bits.address)
+    when(cmdArbiterInst.io.out.bits.write) {
+      rdwrPort := cmdArbiterInst.io.out.bits.wData
+    }
+      .otherwise {
+        memDataOut := rdwrPort
+      }
+  }
+  cmdArbiterInst.io.out.ready := 1.U
+  rData := DontCare
+  rData(0).bits <> memDataOut
+  rData(1).bits <> memDataOut
+  rData(2).bits <> memDataOut
+  rData(3).bits <> memDataOut
+  rData(4).bits <> memDataOut
+  rData(5).bits <> memDataOut
+  rData(6).bits <> memDataOut
+  rData(7).bits <> memDataOut
+  rData(arbChosenPipe).valid := 1.U
 }
