2909d64963d819d4c8088c5d689e985e05dd65f5
Aftab Usmani <m_usmani@apple.com>
2023-11-29 15:55:48 -0800

    circt based RTL generation.

--- src/main/scala/clkDivider/main.scala
+++ src/main/scala/clkDivider/main.scala
@@ -2,7 +2,29 @@ package clkDivider
 
 import chisel3._
 import circt.stage.ChiselStage
-object main extends App {
+object main extends App{
+    val firtoolLoweringOpts = Array(
+        "--lowering-options=disallowPortDeclSharing",
+        "disallowExpressionInliningInPorts",
+        "disallowLocalVariables",
+        "exprInEventControl",
+        "locationInfoStyle=none",
+        "emitWireInPorts",
+        "emittedLineLength=200",
+        "wireSpillingNamehintTermLimit=50",
+        "omitVersionComment",
+        "disallowPackedStructAssignments"
+    )
 
-    circt.stage.ChiselStage.emitSystemVerilog(new clkDivider(divisionValue = 4))
+    val firtoolOpts = Array(
+        "--disable-all-randomization",
+        "--disable-annotation-unknown",
+        "--emit-separate-always-blocks",
+        "--emit-chisel-asserts-as-sva",
+        "--split-verilog",
+        firtoolLoweringOpts.mkString(","), //lowering options are separated by comma
+        "-O=release",
+        "-o=rtl"
+    )
+    circt.stage.ChiselStage.emitSystemVerilogFile(new clkDivider(divisionValue = 5), Array(), firtoolOpts)
 }
--- src/main/scala/gcd/main.scala
+++ src/main/scala/gcd/main.scala
@@ -1,8 +1,28 @@
 package gcd
-import chisel3._
-import chisel3.util._
-import chisel3.stage._
-object main extends App {
 
-  //(new ChiselStage) emitVerilog(new DecoupledGcd(16))
+object main extends App{
+  val firtoolLoweringOpts = Array(
+    "--lowering-options=disallowPortDeclSharing",
+    "disallowExpressionInliningInPorts",
+    "disallowLocalVariables",
+    "exprInEventControl",
+    "locationInfoStyle=none",
+    "emitWireInPorts",
+    "emittedLineLength=200",
+    "wireSpillingNamehintTermLimit=50",
+    "omitVersionComment",
+    "disallowPackedStructAssignments"
+  )
+
+  val firtoolOpts = Array(
+    "--disable-all-randomization",
+    "--disable-annotation-unknown",
+    "--emit-separate-always-blocks",
+    "--emit-chisel-asserts-as-sva",
+    "--split-verilog",
+    firtoolLoweringOpts.mkString(","), //lowering options are separated by comma
+    "-O=release",
+    "-o=rtl"
+  )
+  circt.stage.ChiselStage.emitSystemVerilogFile(new DecoupledGcd(16), Array(), firtoolOpts)
 }
